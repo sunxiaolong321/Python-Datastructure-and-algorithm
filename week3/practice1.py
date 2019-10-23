7754940d0b3024681207e054af0438916f45415d
sunxiaolong <sxl471802552@gamil.com>
2019-10-23 23:28:56 +0800

    modify files

--- week3/practice1.py
+++ week3/practice1.py
@@ -17,14 +17,16 @@
 
 
 # def func(S):
-#     charList = list(S)
-#     for i in range(len(charList)-1):
-#         charList.append(charList.pop(0))
-#     return "".join(charList)
+#     minStr = S
+#     for i in range(len(S)-1):
+#         S = "".join([S[1:],S[0]])
+#         if minStr > S:
+#             minStr = S
+#     return minStr
 
-# if __name__ == "__main__":
-#     S = eval(input())
-#     print(func(S))
+
+# S = eval(input())
+# print(func(S))
 
 """
 题目内容：
@@ -42,12 +44,12 @@
 
 
 # def func(mylist):
-#     # your code here
-#     resultList = []
-#     for element in mylist:
-#         count = 0
-#         for value in mylist:
-#             if value <= element and value >= element - 10000:
+
+#     resultList = list()
+#     for i in range(len(mylist)):
+#         count = 1
+#         for j in range(i):
+#             if mylist[j] <= mylist[i] and mylist[j] >= mylist[i] - 10000:
 #                 count += 1
 #         resultList.append(count)
 #     return resultList
@@ -55,3 +57,35 @@
 
 # mylist = eval(input())
 # print(func(mylist))
+
+
+"""
+题目内容：
+    实现一个基数排序算法，用于10进制的正整数从小到大的排序。
+    思路是保持10个队列(队列0、队列1......队列9、队列main)，
+    开始，所有的数都在main队列，没有排序。
+    第一趟将所有的数根据其10进制个位(0~9)，放入相应的队列0~9，
+    全放好后，按照FIFO的顺序，将每个队列的数合并排到main队列。
+    第二趟再从main队列队首取数，根据其十位的数值，放入相应队列0~9，
+    全放好后，仍然按照FIFO的顺序，将每个队列的数合并排到main队列。
+    第三趟放百位，再合并；第四趟放千位，再合并。
+    直到最多的位数放完，合并完，这样main队列里就是排好序的数列了。
+输入格式:
+    一个列表mylist，其中mylist包含一些需要排序的正整数，
+    正整数互不相同且均不超过100000，且个数在1至1000之间。
+输出格式：
+    一个与mylist等长的列表。
+输入样例：
+    [8, 91, 34, 22, 65, 30, 4, 55, 18]
+输出样例：
+    [4, 8, 18, 22, 30, 34, 55, 65, 91]
+"""
+
+
+class func(mylist):
+    dictItem = {}
+    
+
+
+mylist = eval(input())
+print(func(mylist))
