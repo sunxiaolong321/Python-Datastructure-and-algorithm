37f94454fd3b6e5404959007986710e8e81b924c
sunxiaolong <sxl471802552@gamil.com>
2019-10-28 00:27:22 +0800

    add unordered list

---
+++ week3/List.py
@@ -0,0 +1,142 @@
+# -*- coding:utf-8 -*
+
+
+class Node:     # 节点实现
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getCurrent(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def addCurrent(self, item):
+        self.data = item
+
+    def addNext(self, item):
+        self.next = item
+
+
+class unorderedList:     # 无序列表实现
+    def __init__(self):
+        self.head = None
+
+    def printer(self):
+        current = self.head
+        while current != None:
+            print(current.getCurrent(), end="\t")
+            current = current.getNext()
+
+    def append(self, item):
+        temp = Node(item)
+        temp.addNext(self.head)
+        self.head = temp
+
+    def isEmpty(self):  # 判断是否为空
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):  # 搜索函数
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getCurrent() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def pop(self):  # pop()函数
+        if self.head != None:
+            self.head = self.head.getNext()
+        else:
+            return None
+
+    def remove(self, item):
+        current = self.head
+
+        previous = None
+        found = False
+        while current != None and not found:
+            if current.getCurrent() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:  # 直接找到
+            self.head = current.getNext()
+        elif current == None:  # 未找到
+            return None
+        else:
+            previous.addNext(current.getNext())
+
+    def pop(self, pos):
+        current = self.head
+        previous = None
+        count = 0
+        found = False
+        while current != None and not found:
+            if count == pos:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+                count += 1
+
+        if previous== None:  # 链表未搜索到情况和链表为空情况一样
+            self.head = current.getNext()
+        elif current == None:
+            return None
+        else:
+            previous.addNext(current.getNext())
+
+    def index(self):
+        count = 0
+        current = self.head
+        while current != None:
+            current.getNext()
+            count += 1
+        
+        return count
+
+    def insert(self, pos, item):
+        current = self.head
+        previous = None
+        found = False
+        count = 0
+        while current!= None and not found:
+            if count == pos:
+                found = True
+            else:
+                previous =current
+                current = current.getNext()
+                count += 1
+
+        temp = Node(item)
+        if previous == None:
+            self.head = temp
+            temp.addNext(current)
+        elif current == None:
+            return None
+        else:
+            previous.addNext(temp)
+            temp.addNext(current)
+
+if __name__ == "__main__":
+    x = unorderedList()
+    x.append(1)
+    x.append(2)
+    x.append(4)
+    x.insert(5,6)
+    x.printer()
